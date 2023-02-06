fe7840f2eef1a75f9e3577e50f97b42f73c6f8cf
Drew Marshall <thedrewguy@gmail.com>
2023-02-05 22:52:27 -0600

    did stuff

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
-const wordList = require('./words').map(word => word.toUpperCase());
+import getPrompt from 'prompt-sync';
+const prompt = getPrompt();
+
+import aw from './answer-words.js';
+const answerWords = aw.map(word => word.toUpperCase());
+
+import gw from './guess-words.js';
+const guessWords = gw.map(word => word.toUpperCase());
+
+import chalk from 'chalk';
 
 const colors = {
   green: 'green',
@@ -6,20 +15,180 @@ const colors = {
   grey: 'grey',
 };
 
-const wordMap = makeWordMap(wordList);
+const answerLetterCounts = countLetters(answerWords);
 
-const guess = {};
-guess.word = 'PUPPY';
-guess.coloring = [
-  colors.green,
-  colors.grey,
-  colors.grey,
-  colors.grey,
-  colors.orange,
-];
-guess.constraints = getConstraints(guess);
+const guesses = [];
 
-const validWords = wordList.filter(word => fitsGuess(word, guess));
+let win = false;
+while (!win) {
+  win = loop();
+}
+console.log('');
+guesses.forEach(guess => printGuess(guess));
+console.log('\nyou win');
+console.log(`${guesses.length} guesses`);
+
+function loop() {
+  console.log('');
+  printKeyboard();
+  console.log('');
+  guesses.forEach(guess => printGuess(guess));
+
+  const guess = getGuess();
+  if (!guess) {
+    console.log('enter a real word loser');
+    return;
+  }
+  guesses.push(guess);
+  if (guess.coloring.every(color => color === colors.green)) {
+    return true;
+  }
+  return false;
+}
+
+function printKeyboard() {
+  const rows = [
+    'QWERTYUIOP'.split(''),
+    'ASDFGHJKL'.split(''),
+    'ZXCVBNM'.split(''),
+  ];
+  rows.forEach(row => {
+    let rowString = '';
+    row.forEach(letter => {
+      let letterColor = undefined;
+      guesses.forEach(guess => {
+        guess.word.split('').forEach((guessLetter, index) => {
+          if (guessLetter !== letter) {
+            return;
+          }
+          if (letterColor === colors.green) {
+            return;
+          }
+          if (guess.coloring[index] === colors.green) {
+            letterColor = colors.green;
+            return;
+          }
+          if (letterColor === colors.orange) {
+            return;
+          }
+          if (guess.coloring[index] === colors.orange) {
+            letterColor = colors.orange;
+            return;
+          }
+          if (guess.coloring[index] === colors.grey) {
+            letterColor = colors.grey;
+          }
+        });
+      });
+      if (letterColor === colors.green) {
+        rowString += chalk.green(letter);
+        return;
+      }
+      if (letterColor === colors.orange) {
+        rowString += chalk.orange(letter);
+        return;
+      }
+      if (letterColor === colors.grey) {
+        rowString += chalk.red(letter);
+        return;
+      }
+      rowString += letter;
+    });
+    console.log(rowString);
+  });
+}
+
+function printGuess(guess) {
+  const word = guess.word;
+  let toPrint = '';
+  word.split('').forEach((letter, index) => {
+    if (guess.coloring[index] === colors.green) {
+      toPrint += chalk.green(letter);
+      return;
+    }
+    if (guess.coloring[index] === colors.orange) {
+      toPrint += chalk.rgb(255, 172, 28)(letter);
+      return;
+    }
+    toPrint += letter;
+  });
+  console.log(toPrint);
+}
+
+function getGuess() {
+  const guessWord = prompt('').toUpperCase();
+
+  if (!guessWords.includes(guessWord)) {
+    return;
+  }
+
+  const guess = {};
+  guess.word = guessWord;
+
+  const validAnswers = remainingValidAnswers(guesses);
+  guess.coloring = chooseColoring(validAnswers, guessWord);
+
+  guess.constraints = getConstraints(guess);
+
+  return guess;
+}
+
+function chooseColoring(validAnswers, nextGuess) {
+  const answerColorings = validAnswers.map(word => ({
+    word: word,
+    coloringString: JSON.stringify(answerColoring(nextGuess, word)),
+  }));
+
+  const coloringCounts = {};
+  answerColorings.forEach(({ coloringString }) => {
+    coloringCounts[coloringString] = (coloringCounts[coloringString] ?? 0) + 1;
+  });
+  const commonestColoring = JSON.parse(
+    Object.entries(coloringCounts).sort((a, b) => b[1] - a[1])[0][0]
+  );
+
+  return commonestColoring;
+}
+
+function remainingValidAnswers(guesses) {
+  let validAnswers = [...answerWords];
+  guesses.forEach(guess => {
+    validAnswers = validAnswers.filter(answer => fitsGuess(answer, guess));
+  });
+  return validAnswers;
+}
+
+function answerColoring(guessWord, answer) {
+  const guessLetterCounts = {};
+  const greenColoring = answer.split('').map((answerLetter, index) => {
+    const guessLetter = guessWord.split('')[index];
+    if (guessLetter !== answerLetter) {
+      return;
+    }
+
+    guessLetterCounts[guessLetter] = (guessLetterCounts[guessLetter] ?? 0) + 1;
+
+    return colors.green;
+  });
+
+  const coloring = greenColoring.map((color, index) => {
+    if (color === colors.green) {
+      return colors.green;
+    }
+
+    const guessLetter = guessWord.split('')[index];
+
+    let guessLetterCount = (guessLetterCounts[guessLetter] ?? 0) + 1;
+    guessLetterCounts[guessLetter] = guessLetterCount;
+
+    if (guessLetterCount <= answerLetterCounts.get(answer).get(guessLetter)) {
+      return colors.orange;
+    }
+    return colors.grey;
+  });
+
+  return coloring;
+}
 
 function fitsGuess(word, guess) {
   const letters = word.split('');
@@ -41,7 +210,7 @@ function fitsGuess(word, guess) {
     return false;
   }
 
-  const letterCounts = wordMap.get(word);
+  const letterCounts = answerLetterCounts.get(word);
   const letterCountIsOk = [...letterCounts].every(([letter, count]) => {
     const { min, max } = guess.constraints.get(letter);
     return count >= min && count <= max;
@@ -50,28 +219,26 @@ function fitsGuess(word, guess) {
   return letterCountIsOk;
 }
 
-function makeWordMap(wordList) {
-  const wordMap = new Map();
+function countLetters(wordList) {
+  const wordLetterCounts = new Map();
   wordList.forEach(word => {
-    const letterCounts = new Map();
-    word.split('').forEach(letter => {
-      const letterCount = letterCounts.get(letter);
-      if (letterCount) {
-        letterCounts.set(letter, letterCount + 1);
-        return;
-      }
-      letterCounts.set(letter, 1);
-    });
-    wordMap.set(word, letterCounts);
+    const letterCounts = defaultLetterMap(() => 0);
+
+    word
+      .split('')
+      .forEach(letter =>
+        letterCounts.set(letter, letterCounts.get(letter) + 1)
+      );
+    wordLetterCounts.set(word, letterCounts);
   });
-  return wordMap;
+  return wordLetterCounts;
 }
 
-function defaultLetterConstraints() {
+function defaultLetterMap(getDefault) {
   const constraints = new Map();
   const aCode = 65;
   for (let i = 0; i < 26; i++) {
-    constraints.set(String.fromCharCode(aCode + i), { min: 0, max: 5 });
+    constraints.set(String.fromCharCode(aCode + i), getDefault());
   }
   return constraints;
 }
@@ -94,7 +261,7 @@ function getLetterColorCounts(guess) {
 }
 
 function getConstraints(guess) {
-  const guessConstraints = defaultLetterConstraints();
+  const guessConstraints = defaultLetterMap(() => ({ min: 0, max: 5 }));
 
   const letterColorCounts = getLetterColorCounts(guess);
 
